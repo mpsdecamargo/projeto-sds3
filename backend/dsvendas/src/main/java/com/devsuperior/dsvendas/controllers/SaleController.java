83cf16569b0eecccfb32cda2b88f7afee3ea292d
mpsdecamargo <mpsdecamargo@gmail.com>
2021-05-06 22:58:07 -0300

    First homolog

--- backend/dsvendas/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -27,11 +27,18 @@ public class SaleController {
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
 
 	@GetMapping(value = "/success-by-seller")
 	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
 		List<SaleSuccessDTO> list = service.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
+	
 	
 }
